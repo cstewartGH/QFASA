ab3d6cdd8e2f53f38a827e6fa0961d5d9433bf4a
Connie Stewart <cstewart@unb.ca>
2021-06-02 13:42:29 -0300

    Data for balanced repeatability example.

---
+++ R/bal.diet.data.R
@@ -0,0 +1,23 @@
+#' Sample example of balanced repeatability diet estimates data with only two
+#' repeated measurements per predator.
+#'
+#' @format A data frame with 100 predator diets (50 unique predators) and
+#' 13 variables:
+#' \describe{
+#' \item{Seal.ID}{Predator (1 to 50)}
+#' \item{Year}{Either 1 or 2}
+#' \item{capelin}{estimated diet proportion}
+#' \item{coho}{estimated diet proportion}
+#' \item{eulachon}{estimated diet proportion}
+#' \item{herring}{estimated diet proportion}
+#' \item{mackerel}{estimated diet proportion}
+#' \item{pilchard}{estimated diet proportion}
+#' \item{pollock}{estimated diet proportion}
+#' \item{sandlance}{estimated diet proportion}
+#' \item{squid}{estimated diet proportion}
+#' \item{surfsmelt_s}{estimated diet proportion}
+#' \item{sufsmelt_lg}{estimated diet proportion}
+#' }
+#'
+"bal.diet.data"
+
