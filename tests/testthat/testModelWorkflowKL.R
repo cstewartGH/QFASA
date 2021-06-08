70d4e55ad99d7fa9fea82525fc2865ab618d4a6d
Connie Stewart <cstewart@unb.ca>
2021-06-08 12:19:13 -0300

    Added and then removed a library.

--- tests/testthat/testModelWorkflowKL.R
+++ tests/testthat/testModelWorkflowKL.R
@@ -6,7 +6,7 @@ test_that("Model Workflow Regression KL", {
     ## Fatty Acids
     data(FAset)
     fa.set = as.vector(unlist(FAset))
-    
+
     ## Predators
     data(predatorFAs)
     tombstone.info = predatorFAs[,1:4]
@@ -16,12 +16,12 @@ test_that("Model Workflow Regression KL", {
     ## Prey
     data(preyFAs)
     prey.sub=(preyFAs[,4:(ncol(preyFAs))])[fa.set]
-    prey.sub=prey.sub/apply(prey.sub,1,sum) 
+    prey.sub=prey.sub/apply(prey.sub,1,sum)
     group=as.vector(preyFAs$Species)
     prey.matrix=cbind(group,prey.sub)
-    prey.matrix=MEANmeth(prey.matrix) 
+    prey.matrix=MEANmeth(prey.matrix)
 
-    FC = preyFAs[,c(2,3)] 
+    FC = preyFAs[,c(2,3)]
     FC = as.vector(tapply(FC$lipid,FC$Species,mean,na.rm=TRUE))
 
     ## Calibration Coefficients
