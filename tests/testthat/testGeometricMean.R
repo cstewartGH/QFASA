0135646df16658dc0f7705826fabff917a9958c2
cstewartGH <cstewart@unb.ca>
2023-11-16 16:04:13 -0400

    mean.geometric to mean_geometric

--- tests/testthat/testGeometricMean.R
+++ tests/testthat/testGeometricMean.R
@@ -1,5 +1,5 @@
 test_that("Geometric mean calculation", {
-    
+
     x <- c(1,2,3,4,5,6,7,8,9)
-    expect_equal(QFASA:::mean.geometric(x), exp(mean(log(x))))
+    expect_equal(QFASA:::mean_geometric(x), exp(mean(log(x))))
 })
